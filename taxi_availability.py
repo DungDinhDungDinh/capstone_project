eba1514e53c1f99e6b2a32ea148f67c2c3162ec0
Dung Dinh <dungdinh@Dungs-MacBook-Pro.local>
2023-10-01 09:32:13 +0800

    Getting Sentosa 21-day data

--- taxi_availability.py
+++ taxi_availability.py
@@ -885,8 +885,8 @@ def writingToCSVFile():
     save_path = 'sentosa'
     
     #EDIT 2 PLACES !!!!
-    date = '2023-04-11'
-    day_type = 'weekday'
+    date = '2023-06-29'
+    day_type = 'holiday'
     
     hours = ['00','01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
     
